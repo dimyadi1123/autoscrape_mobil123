ddf38f60ef4bd6172287df4b75d42d3f9aac2ab8
capybarathrum <dimyadi1123@gmail.com>
2024-06-13 19:27:49 +0700

    percobaan terakhir

--- automatic_scrape.py
+++ automatic_scrape.py
@@ -9,6 +9,33 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
+import logging
+import logging.handlers
+import os
+import requests
+
+import logging
+import logging.handlers
+import os
+
+import requests
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+try:
+    SOME_SECRET = os.environ["SOME_SECRET"]
+except KeyError:
+    SOME_SECRET = "Token not available!"
 
 # Konfigurasi Selenium WebDriver
 options = webdriver.ChromeOptions()
@@ -136,6 +163,8 @@ def load_and_update_data():
         combined_data.to_csv('car_data.csv', index=False)
 
 if __name__ == '__main__':
+    logger.info(f"Token value: {SOME_SECRET}")
+
     # Scraping dua halaman pertama sebagai contoh
     for page in range(1, 3):
         scrape_page(page)
